e93dbbe5461e8a18f907ed9708f09f32b3c85036
adriton <adrtler@gmail.com>
2020-11-21 11:02:48 +0100

    Editar nombres en la tabla

--- src/components/BandList.js
+++ src/components/BandList.js
@@ -1,23 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-export const BandList = () => {
+export const BandList = ({ data }) => {
+  const [bands, setBands] = useState(data);
+
+  useEffect(() => {
+    setBands(data);
+  }, [data]);
+
+  const cambioNombre = (event, id) => {
+    const nuevoNombre = event.target.value;
+    setBands((bands) =>
+      bands.map((band) => {
+        if (band.id === id) {
+          band.name = nuevoNombre;
+        }
+        return band;
+      })
+    );
+  };
+
+  const onPerdioFoco = (id, nombre) => {
+    console.log(id, nombre);
+    // TODO: Disparar el evento de socket
+  };
+
   const crearRows = () => {
-    return (
-      <tr>
+    return bands.map((band) => (
+      <tr key={band.id}>
         <td>
           <button className="btn btn-primary">+1</button>
         </td>
         <td>
-          <input className="form-control" />
+          <input
+            className="form-control"
+            value={band.name}
+            onChange={(event) => cambioNombre(event, band.id)}
+            onBlur={() => onPerdioFoco(band.id, band.name)}
+          />
         </td>
         <td>
-          <h3>15</h3>
+          <h3>{band.votes}</h3>
         </td>
         <td>
           <button className="btn btn-danger">Borrar</button>
         </td>
       </tr>
-    );
+    ));
   };
   return (
     <>
